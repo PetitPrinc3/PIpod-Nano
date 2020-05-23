7600940b8c971476a9cfd7aab76eee9ba5e0c20f
Gavroche <64252014+G-a-v-r-o-c-h-e@users.noreply.github.com>
2020-05-23 18:20:13 +0200

    Update and rename powerbutton.py to buttons.py

--- buttons.py
+++ buttons.py
@@ -12,5 +12,16 @@ while True:
                         duration = time.time() - tmp
                         if duration > 3:
                                 os.system("shutdown now -h")
+        if volumeUp.is_pressed:
+                time.sleep(.25)
+                while volumeUp.is_pressed:
+                        os.system("mpc volume +1")
+                        time.sleep(.25)
+
+        if volumeDown.is_pressed:
+                time.sleep(.25)
+                while volumeDown.is_pressed:
+                        os.system("mpc volume -1")
+                        time.sleep(.25)
 
         time.sleep(1)
